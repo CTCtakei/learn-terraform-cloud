a542c54e9339a795507eba7ea3c537c8bd549c40
ctctakei <yuya.takei@ctc-g.co.jp>
2023-06-29 19:12:42 +0900

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "CTCtakei"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
